8ac9d42b9c1cbfadd6f4324f45af38087442da8d
Sgiane11a <steyci.cordova@tecsup.edu.pe>
2025-05-28 14:33:00 -0500

    Imagen de categoria agregado

--- api/models.py
+++ api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
